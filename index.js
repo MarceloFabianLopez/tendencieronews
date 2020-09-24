94906d52ea3851484b9b7c181ffefb0b007a3efb
Marcelo Fabián López <cmflopez@gmail.com>
2020-09-24 08:27:46 -0300

    funciona cors en local

--- index.js
+++ index.js
@@ -1,10 +1,69 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+//import MiComponent from './src/componennts/MiComponente';
+var cors = require("cors");
+const googleTrends = require('google-trends-api');
 
+function diaria(){
+    let currentComponent = this;
+    var salida = [];
+    var fecha =Date();
+    googleTrends.dailyTrends({
+            trendDate: fecha,
+            hl :'es',
+            geo: 'AR'}).then(function(results)
+                            {  JSON.parse(results).default.trendingSearchesDays.forEach(s => {
+                                        salida.push(s);
+                                        console.log('salida dentro de daily',s['formattedDate']);}
+                              )
+                              //currentComponent.cambiarEstado(salida);
+                              console.log('salida=',salida);
+                              return (salida);
+                             } 
+                             ).catch(function(err)
+                                                  { console.error('Oh no there was an error', err);
+                                                  }
+                                    )
+    }
+
+function miapi(){
+  return ('resultado mi api');
+}
 express()
+   .use(cors())
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/daily',(req,res) => {
+     // res.send('daily.....................'+miapi()+diaria());
+      console.log('dentro de get daily :');
+      //diaria();
+      var salida = [];
+      var fecha =Date();
+      googleTrends.dailyTrends({
+              trendDate: fecha,
+              hl :'es',
+              geo: 'AR'}).then(function(results)
+                              {  JSON.parse(results).default.trendingSearchesDays.forEach(s => {
+                                          salida.push(s);
+                                          console.log('salida dentro de daily',s['formattedDate']);}
+                                )
+                                //currentComponent.cambiarEstado(salida);
+                                console.log('salida=',salida);
+                                //return (salida);
+                                res.send(salida);
+                               } 
+                               ).catch(function(err)
+                                                    { console.error('Oh no there was an error', err);
+                                                    res.send('error api');
+                                                    }
+                                      )
+   
+   
+   
+    })
+
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ src/componennts/MiComponente.js
@@ -0,0 +1,30 @@
+import React,{Component} from 'react';
+//import JSONViewer from 'react-json-viewer';
+//import ListaBusquedas from '../components/ListaBusquedas';
+//const googleTrends = require('google-trends-api');
+
+class MiComponent extends Component {
+ 
+ 
+ constructor(props) {
+  super(props)
+  this.state = {
+    unjson: {}
+  }
+}  
+ cambiarEstado(dato){
+   this.setState({
+     unjson :dato
+   })
+ }       DailyTrends
+      
+    render() {
+      
+        <div>
+        
+      Hola mundo       
+       </div>
+      }
+    
+      }
+  export default MiComponent;
